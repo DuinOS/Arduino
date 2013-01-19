196ada7596f8aa9b0ab2a7e4829a17381436120b
Carlos Delfino Carvalho Pinheiro <consultoria@carlosdelfino.eti.br>
2013-01-19 10:14:33 -0200

    atualizando com as últimas correções do master (ide 1.0.x)

--- hardware/arduino/cores/duinos/main.cpp
+++ hardware/arduino/cores/duinos/main.cpp
@@ -1,6 +1,13 @@
 #include <Arduino.h>
+#include <FreeRTOSConfig.h>
 
-unsigned portBASE_TYPE mainLoopPriority;
+unsigned portBASE_TYPE mainSetupPriority  = HIGH_PRIORITY;
+unsigned portBASE_TYPE mainSetupStackSize = configMINIMAL_STACK_SIZE * 2;
+unsigned portBASE_TYPE mainLoopPriority   = LOW_PRIORITY;
+unsigned portBASE_TYPE mainLoopStackSize  = configMINIMAL_STACK_SIZE;
+
+xTaskHandle xHandleLoop;
+xTaskHandle xHandleSetup;
 
 void main_Task(void *pvParameters)
 {
@@ -8,26 +15,39 @@ void main_Task(void *pvParameters)
 		loop();
 		if (serialEventRun) serialEventRun();
 	}
-        
+		
+} 
+
+void setup_Task(void *pvParameters)
+{
+
+	setup();
+
+	xTaskCreate(main_Task, (signed portCHAR *) "main", mainLoopStackSize, NULL, mainLoopPriority, &xHandleLoop);
+
+#if INCLUDE_vTaskDelete
+	vTaskDelete(xHandleSetup);
+#else
+	while(true) vTaskSuspend(NULL);
+#endif
+
 }
 
 int main(void) 
 {
-	mainLoopPriority = LOW_PRIORITY;
 
 	init();
 
 #if defined(USBCON)
 	USBDevice.attach();
 #endif
-	
-	setup();
 
-	xTaskCreate(main_Task, (signed portCHAR *) "main", configMINIMAL_STACK_SIZE, NULL, mainLoopPriority, NULL);
+	xTaskCreate(setup_Task, (signed portCHAR *) "setup", mainSetupStackSize, NULL, mainSetupStackSize, &xHandleSetup);
+
 	vTaskStartScheduler();
 
-	for(;;);
-
+	for (;;);
+        
 	return 0;
 }
 
