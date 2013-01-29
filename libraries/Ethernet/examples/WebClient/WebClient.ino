77c48c99832bdc97dfe8391a642dc8aac908dc0b
Carlos Delfino Carvalho Pinheiro <consultoria@carlosdelfino.eti.br>
2013-01-28 22:09:35 -0200

    imprime o IP adquirido no DHCP.

--- libraries/Ethernet/examples/WebClient/WebClient.ino
+++ libraries/Ethernet/examples/WebClient/WebClient.ino
@@ -18,7 +18,8 @@
 
 // Enter a MAC address for your controller below.
 // Newer Ethernet shields have a MAC address printed on a sticker on the shield
-byte mac[] = {  0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+byte mac[] = {  
+    0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
 IPAddress server(173,194,33,104); // Google
 
 // Initialize the Ethernet client library
@@ -27,54 +28,59 @@ IPAddress server(173,194,33,104); // Google
 EthernetClient client;
 
 void setup() {
- // Open serial communications and wait for port to open:
-  Serial.begin(9600);
-   while (!Serial) {
-    ; // wait for serial port to connect. Needed for Leonardo only
-  }
+    // Open serial communications and wait for port to open:
+    Serial.begin(9600);
+    while (!Serial) {
+        ; // wait for serial port to connect. Needed for Leonardo only
+    }
 
-  // start the Ethernet connection:
-  if (Ethernet.begin(mac) == 0) {
-    Serial.println("Failed to configure Ethernet using DHCP");
-    // no point in carrying on, so do nothing forevermore:
-    for(;;)
-      ;
-  }
-  // give the Ethernet shield a second to initialize:
-  delay(1000);
-  Serial.println("connecting...");
+    // start the Ethernet connection:
+    if (Ethernet.begin(mac) == 0) {
+        Serial.println("Failed to configure Ethernet using DHCP");
+        // no point in carrying on, so do nothing forevermore:
+        for(;;)
+            ;
+    }
+    // give the Ethernet shield a second to initialize:
+    delay(1000);
+    Serial.println("connecting...");
 
-  // if you get a connection, report back via serial:
-  if (client.connect(server, 80)) {
-    Serial.println("connected");
-    // Make a HTTP request:
-    client.println("GET /search?q=arduino HTTP/1.0");
-    client.println();
-  } 
-  else {
-    // kf you didn't get a connection to the server:
-    Serial.println("connection failed");
-  }
+    Serial.print("IP: ");
+    Serial.println(Ethernet.localIP());
+    
+
+    // if you get a connection, report back via serial:
+    if (client.connect(server, 80)) {
+        Serial.println("connected");
+        // Make a HTTP request:
+        client.println("GET /search?q=arduino HTTP/1.0");
+        client.println();
+    } 
+    else {
+        // kf you didn't get a connection to the server:
+        Serial.println("connection failed");
+    }
 }
 
 void loop()
 {
-  // if there are incoming bytes available 
-  // from the server, read them and print them:
-  if (client.available()) {
-    char c = client.read();
-    Serial.print(c);
-  }
+    // if there are incoming bytes available 
+    // from the server, read them and print them:
+    if (client.available()) {
+        char c = client.read();
+        Serial.print(c);
+    }
 
-  // if the server's disconnected, stop the client:
-  if (!client.connected()) {
-    Serial.println();
-    Serial.println("disconnecting.");
-    client.stop();
+    // if the server's disconnected, stop the client:
+    if (!client.connected()) {
+        Serial.println();
+        Serial.println("disconnecting.");
+        client.stop();
 
-    // do nothing forevermore:
-    for(;;)
-      ;
-  }
+        // do nothing forevermore:
+        for(;;)
+            ;
+    }
 }
+
 
